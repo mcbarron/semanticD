0fc0cad1ee6753642421570dd2dd04cc21678e7a
Mike Barron <mcbarron>
2016-03-11 11:45:32 -0500

    Yup

--- semantic.js
+++ semantic.js
@@ -1,5 +1,5 @@
 /*** LICENSE *************************************************************************************
- * @title Semantic jQuery Plugin (popluation analysis and decomposition)
+ * @title Semantic popluation analysis and decomposition
  * @author Michael Barron / Bugseed
  * @license Creative Commons Attribution-ShareAlike 3.0 Unported License; to view a copy of this
  *  ___ ____ license, visit http://creativecommons.org/licenses/by-sa/3.0/
@@ -9,8 +9,9 @@
  *
  *************************************************************************************************/
 
-var $ = require("jquery");
- 
+#var $ = require("jquery");
+var BreakdownBy = require("BreakdownBy.js");
+
 /**
  * - BASIC DEFINITIONS ------------------------------------------------------------------------------------------
  */
@@ -19,68 +20,6 @@ if (typeof String.prototype.startsWith === "undefined") {
 }
 if (typeof String.prototype.endsWith === "undefined") {
 	String.prototype.endsWith = function(suffix) { return this.indexOf(suffix, this.length - suffix.length) !== -1; };
-}
-
-/**
- * --------------------------------------------------------------------------------------------------------------
- */
-function BreakdownBy(breakdownAttribute) {
-	this.breakdownAttribute = breakdownAttribute;
-	this.size = 0;
-	this.popLookup = {};
-	this.pops = [];
-	this.keys = [];
-}
-BreakdownBy.prototype.getSize = function() {	
-	return this.size;
-}
-BreakdownBy.prototype.getBreakdownAttribute = function() {	
-	return this.breakdownAttribute;
-}
-BreakdownBy.prototype.get = function(key, addIfNotPresent) {
-	if (addIfNotPresent && !this.popLookup.hasOwnProperty(key)) {
-		this.size++;
-		this.pops.push(this.popLookup[key] = new Population());
-		this.keys.push(key);
-	}
-	return this.popLookup[key];
-}
-BreakdownBy.prototype.getKeys = function() {
-	return this.keys;
-}
-BreakdownBy.prototype.getPops = function() {
-	return this.pops;
-}
-BreakdownBy.prototype.getOrderedKeys = function(fixedOrder) {
-	if (fixedOrder !== undefined) {
-		var i, forcedSort = [], regularSort = [];
-		for (i = 0; i < fixedOrder.length; i++) {
-			if (this.popLookup[fixedOrder[i]] !== undefined) {
-				forcedSort.push(fixedOrder[i]);
-			}
-		}
-		for (i = 0; i < this.keys.length; i++) {
-			if (fixedOrder !== undefined) {
-				if (jQuery.inArray(this.keys[i], forcedSort) < 0) {
-					// only insert to return array if it doesn't already exist
-					regularSort.push(this.keys[i]);
-				}
-			} else {
-				// always push
-				regularSort.push(this.keys[i]);
-			}
-		}
-		return forcedSort.concat(regularSort.sort());
-	} else {
-		return this.keys.slice(0).sort();
-	}
-}
-BreakdownBy.prototype.getPopSizes = function() {
-	var rv = {};
-	for (var i = 0; i < this.pops.length; i++) {
-		rv[this.keys[i]] = this.pops[i].getSize();
-	}
-	return rv;
 }
 
 /**
